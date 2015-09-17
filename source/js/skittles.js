31a29186e25a47f271097c18a165a24e0c29cb79
Joe Mainwaring <joe@highground.com>
2015-09-17 16:21:01 -0500

    fully functional prototype

--- source/js/skittles.js
+++ source/js/skittles.js
@@ -1,6 +1,6 @@
-var app = angular.module('skittles', ['angular-clipboard']);
+var app = angular.module('skittles', ['ngNotificationsBar']);
 
-app.controller('paletteCtrl', ['$scope', '$document', function ($scope, $document) {
+app.controller('paletteCtrl', ['$scope', '$document', 'notifications', function ($scope, $document, notifications) {
 
   var colorOutput = {};
 
@@ -76,13 +76,29 @@ app.controller('paletteCtrl', ['$scope', '$document', function ($scope, $documen
     {name: 'turquoise', hex: '#1abc9c'},
     {name: 'green sea', hex: '#16a085'},
     {name: 'emerald', hex: '#2ecc71'},
-    {name: 'nephritis', hex: '#27ae60'}
+    {name: 'nephritis', hex: '#27ae60'},
+    {name: 'peter river', hex: '#3498db'},
+    {name: 'belize hole', hex: '#2980b9'},
+    {name: 'amethyst', hex: '#9b59b6'},
+    {name: 'wisteria', hex: '#8e44ad'},
+    {name: 'wet asphalt', hex: '#34495e'},
+    {name: 'midnight blue', hex: '#2c3e50'},
+    {name: 'sunflower', hex: '#f1c40f'},
+    {name: 'orange', hex: '#f39c12'},
+    {name: 'carrot', hex: '#e67e22'},
+    {name: 'pumpkin', hex: '#d35400'},
+    {name: 'alizarin', hex: '#e74c3c'},
+    {name: 'pomegranate', hex: '#c0392b'},
+    {name: 'clouds', hex: '#ecf0f1'},
+    {name: 'silver', hex: '#bdc3c7'},
+    {name: 'concrete', hex: '#95a5a6'},
+    {name: 'asbestos', hex: '#7f8c8d'}
   ];
 
   $scope.copyColor = function (color) {
     var colorValue = colorOutput[$scope.outputFormat](color);
     copyText(colorValue);
-    console.log('color copied:', colorValue);
+    notifications.showSuccess({message: 'Color copied to clipboard!'});
   };
 
   $scope.success = function (color) {
@@ -92,4 +108,14 @@ app.controller('paletteCtrl', ['$scope', '$document', function ($scope, $documen
   $scope.init = function () {
     $scope.outputFormat = $scope.formats[0].value;
   };
+
+  $scope.colorText = function (name, hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex),
+        r = parseInt(result[1], 16),
+        g = parseInt(result[2], 16),
+        b = parseInt(result[3], 16),
+        luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    console.log(name, luma);
+    return (luma < 75);
+  }
 }]);
