16c8abda411aafd8cbec2aa47650d2d064a0d8d7
Joe Mainwaring <jm@theaccordance.com>
2016-07-26 12:38:08 -0400

    reorg'd color-chip component under palette feature

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -2,7 +2,7 @@ define([
     'angular',
     'app/core/palettes.constant',
     'app/palette/palette.component',
-    'app/color-chip/color.chip.component',
+    'app/palette/color-chip/color.chip.component',
     'app/layout/page-corner/page.corner.component',
     'app/core/main.ctrl'
 
